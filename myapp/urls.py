29335a4bee26061847f10e51d20a14680c4c9724
Mitchell-Kirimi <kirimimitchell@gmail.com>
2025-03-03 13:38:27 +0300

    initial commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from.import views
+from django.urls  import path
+from . import views
 
 urlpatterns=[
-    path('',views.index,name='index')
+path('',views.User_list),
+path('Add/',views.adduser)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, redirect
+from yt_dlp.utils import args_to_str
 
+from .models import User
+from .forms import UserForm
 # Create your views here.
-def index (request):
-    post=User.objects.all()
-    return render(request,'index.html')
+def User_list(request):
+    users=User.objects.all()
+    return render(request,'listingpage.html',{'users':users})
+
+def adduser(request):
+    mydict={}
+    form = UserForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    mydict['form']= form
+    return render(request, 'add.html', mydict)
+
